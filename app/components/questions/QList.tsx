611b7809c485b5f5ceff281e6c203ac5c7cd003d
Pedram Ghane <p.ghane@gmail.com>
2025-07-03 22:42:44 +0200

    style: form responsive layout

--- app/components/questions/QList.tsx
+++ app/components/questions/QList.tsx
@@ -1,24 +1,33 @@
+import { Avatar, AvatarImage } from "../ui/avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { QuestionFormData } from "./QForm";
 
 export const QuestionsList = ({ data }: { data: QuestionFormData[] }) => {
   return (
-    <section className="py-6">
+    <section className="pb-12 pt-6">
       <div className="container mx-auto max-w-6xl px-4 md:px-1.5">
         {data.length > 0 ? (
           <div className="flex flex-col gap-4">
             {data.map((item, index) => (
-              <Card key={index}>
-                <CardHeader>
-                  <CardTitle>
-                    <div>
-                      <p>{item.name}</p>
-                      <p>{item.email}</p>
+              <Card
+                key={index}
+                className="border-2 border-gray-200/75 p-5 shadow-none md:px-8 md:py-6"
+              >
+                <CardHeader className="mb-3 p-0">
+                  <CardTitle className="flex items-center gap-3">
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                    </Avatar>
+                    <div className="text-base">
+                      <p className="font-normal text-gray-700">{item.name}</p>
+                      <p className="text-sm font-light text-gray-400">
+                        {item.email}
+                      </p>
                     </div>
                   </CardTitle>
                 </CardHeader>
-                <CardContent>
-                  <h2>{item.title}</h2>
+                <CardContent className="p-0">
+                  <h2 className="text-lg font-semibold">{item.title}</h2>
                   <p>{item.question}</p>
                 </CardContent>
               </Card>
